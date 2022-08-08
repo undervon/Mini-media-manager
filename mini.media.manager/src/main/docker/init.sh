a629604d4621456c7660a77fd938cf6eed64ecfa
undervon <silviu.undervon@gmail.com>
2022-08-08 13:13:55 +0300

    Adding bash script in docker-compose

---
+++ mini.media.manager/src/main/docker/init.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+printf "Configuring resources...\n"
+
+ACCESS_KEY="test-access-key"
+SECRET_ACCESS_KEY="test-secret-access-key"
+LOCALSTACK_URL="http://localhost:4566"
+
+set -x
+
+# set the AWS variables
+aws configure set aws_access_key_id "$ACCESS_KEY"
+aws configure set aws_secret_access_key "$SECRET_ACCESS_KEY"
+aws configure set region eu-west-1
+aws configure set output json
+
+# print the AWS configuration list
+aws configure list
+
+# waiting for tables and bucket to be created
+sleep 1
+aws s3 mb s3://media-bucket --endpoint-url "$LOCALSTACK_URL"
+aws dynamodb create-table --cli-input-json file:////docker-entrypoint-initdb.d/post_table.json --endpoint-url "$LOCALSTACK_URL"
+aws dynamodb create-table --cli-input-json file:////docker-entrypoint-initdb.d/user_table.json --endpoint-url "$LOCALSTACK_URL"
+
+# print resources for validation
+sleep 1
+aws s3 ls --endpoint-url "$LOCALSTACK_URL"
+aws dynamodb list-tables --endpoint-url "$LOCALSTACK_URL"
+aws dynamodb describe-table --table-name posts --endpoint-url "$LOCALSTACK_URL"
+aws dynamodb describe-table --table-name users --endpoint-url "$LOCALSTACK_URL"
+
+echo "Resources created and ready!"
